bf568bbac204e8a0d8fb046fa7161ea6a9a0fa0a
Donny Yung <donaldyung@datawire.io>
2022-01-13 14:32:08 -0500

    Commit since things work
    Signed-off-by: Donny Yung <donaldyung@datawire.io>

--- pkg/client/cli/cliutil/systema.go
+++ pkg/client/cli/cliutil/systema.go
@@ -1,13 +1,22 @@
 package cliutil
 
 import (
+	"bufio"
 	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 
 	grpcCodes "google.golang.org/grpc/codes"
 	grpcStatus "google.golang.org/grpc/status"
 	empty "google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/telepresenceio/telepresence/rpc/v2/connector"
+	"github.com/telepresenceio/telepresence/v2/pkg/client"
 	"github.com/telepresenceio/telepresence/v2/pkg/client/errcat"
 	"github.com/telepresenceio/telepresence/v2/pkg/client/userd/auth/authdata"
 )
@@ -18,7 +27,11 @@ import (
 // the key is used instead of performing an interactive login.
 func EnsureLoggedIn(ctx context.Context, apikey string) (connector.LoginResult_Code, error) {
 	var code connector.LoginResult_Code
-	err := WithConnector(ctx, func(ctx context.Context, connectorClient connector.ConnectorClient) error {
+	_, err := GetTelepresencePro(ctx)
+	if err != nil {
+		return connector.LoginResult_UNSPECIFIED, err
+	}
+	err = WithConnector(ctx, func(ctx context.Context, connectorClient connector.ConnectorClient) error {
 		var err error
 		code, err = ClientEnsureLoggedIn(ctx, apikey, connectorClient)
 		return err
@@ -126,4 +139,54 @@ func GetCloudLicense(ctx context.Context, outputFile, id string) (string, string
 		return "", "", err
 	}
 	return licenseData.GetLicense(), licenseData.GetHostDomain(), nil
+}
+
+func GetTelepresencePro(ctx context.Context) (string, error) {
+	executable, err := os.Executable()
+	if err != nil {
+		return "", errcat.Unknown.Newf("Unable to get path for executable: %s", err)
+	}
+	telProLocation := fmt.Sprintf("%s/telepresence-pro", filepath.Dir(executable))
+	if _, err := os.Stat(telProLocation); os.IsNotExist(err) {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Printf("Telepresence Pro is required to use login features, can Telepresence install it? (y/n)")
+		reply, err := reader.ReadString('\n')
+		if err != nil {
+			return "", err
+		}
+
+		reply = strings.TrimSpace(reply)
+		if reply == "n" {
+			return "", errcat.User.New("Telepresence Pro must be installed to login\n")
+		}
+		// TODO: replace the hardcoded 0.0.1 with this once publishing is working
+		clientVersion := strings.Trim(client.Version(), "v")
+		systemAHost := client.GetConfig(ctx).Cloud.SystemaHost
+		installString := fmt.Sprintf("https://%s/download/tel-pro/%s/%s/0.0.1/telepresence-pro", systemAHost, runtime.GOOS, runtime.GOARCH)
+		fmt.Printf("installing %s version %s to %s\n", installString, clientVersion, telProLocation)
+
+		resp, err := http.Get(installString)
+		if err != nil {
+			return "", errcat.User.Newf("unable to install Telepresence Pro: %s", err)
+		}
+		defer resp.Body.Close()
+
+		out, err := os.Create(telProLocation)
+		if err != nil {
+			return "", errcat.User.Newf("unable to create file %s for Telepresence Pro: %s", telProLocation, err)
+		}
+		defer out.Close()
+
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			return "", errcat.User.Newf("unable to copy Telepresence Pro to %s: %s", telProLocation, err)
+		}
+
+		err = os.Chmod(telProLocation, 0755)
+		if err != nil {
+			return "", errcat.User.Newf("unable to set permissions of Telepresence Pro to 755: %s", err)
+		}
+
+	}
+	return telProLocation, nil
 }
